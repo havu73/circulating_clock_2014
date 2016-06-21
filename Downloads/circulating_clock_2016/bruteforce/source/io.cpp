8ab8cb2990c3fb9c166e30cdd821b145d1c3a12f
aylab <aylab@cu84386m.colgate.edu>
2016-06-21 13:32:28 -0400

    sres works now. Python and matlab tested that Euler really give naN. Trying to brute force parameters

---
+++ Downloads/circulating_clock_2016/bruteforce/source/io.cpp
@@ -0,0 +1,286 @@
+/*
+Stochastically ranked evolutionary strategy sampler for zebrafish segmentation
+Copyright (C) 2013 Ahmet Ay, Jack Holland, Adriana Sperlea, Sebastian Sangervasi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+io.cpp contains functions for input and output of files and pipes. All I/O related functions should be placed in this file.
+*/
+
+#include <sys/wait.h> // Needed for waitpid
+#include <unistd.h> // Needed for pipe, read, write, close, fork, execv
+#include <stdio.h>
+#include "io.hpp" // Function declarations
+
+#include "init.hpp"
+#include "macros.hpp"
+extern input_params ip; // Declared in main.cpp
+extern terminal* term; // Declared in init.cpp
+
+/* store_filename stores the given value in the given field
+	parameters:
+		field: a pointer to the filename's field
+		value: the filename to store
+	returns: nothing
+	notes:
+		The previous field value is freed before assigning the new one.
+	todo:
+*/
+void store_filename (char** field, const char* value) {
+	mfree(*field);
+	*field = copy_str(value);
+}
+
+
+
+
+/* open_file opens the file with the given name and stores it in the given output file stream
+	parameters:
+		file_pointer: a pointer to the output file stream to open the file with
+		file_name: the path and name of the file to open
+		append: if true, the file will appended to, otherwise any existing data will be overwritten
+	returns: nothing
+	notes:
+	todo:
+		TODO: check if this function is still being used
+*/
+void open_file (ofstream* file_pointer, char* file_name, bool append) {
+	ostream& v = term->verbose();
+	try {
+		if (append) {
+			v << term->blue << "Opening " << term->reset << file_name << " . . . ";
+			file_pointer->open(file_name, fstream::app);
+		} else {
+			v << term->blue << "Creating " << term->reset << file_name << " . . . ";
+			file_pointer->open(file_name, fstream::out);
+		}
+	} catch (ofstream::failure) {
+		cout << term->red << "Couldn't write to " << file_name << "!" << term->reset << endl;
+		exit(EXIT_FILE_WRITE_ERROR);
+	}
+	term->done(v);
+}
+
+/* simulate_set performs the required piping to setup and run a simulation with the given parameters
+	parameters:
+		parameters: the parameters to pass as a parameter set to the simulation
+	returns: the score the simulation received
+	notes:
+	todo:
+*/
+double simulate_set (double parameters[]) {
+	ostream& v = term->verbose();
+
+	// Create a pipe
+	int sim_in[2];
+	int sim_out[2]; 
+	
+	v << "  ";
+	v << term->blue << "Creating a pipe " << term->reset << ". . . ";
+	if (pipe(sim_in) == -1 || pipe(sim_out) == -1) {
+		term->failed_pipe_create();
+		exit(EXIT_PIPE_CREATE_ERROR);
+	}
+	v << term->blue << "Done: " << term->reset << "parent_read " << sim_out[0] << " parent_write " << sim_in[1] << " child_write " << sim_out[1] << " child_read "<< sim_in[0] << endl;
+	
+	int parent_read = sim_out[0];
+	int parent_write = sim_in[1];
+	int child_write = sim_out[1];
+	int child_read = sim_in[0];
+	// Copy the user-specified simulation arguments and fill the copy with the pipe's file descriptors
+	char** sim_args = copy_args(ip.sim_args, ip.num_sim_args);
+	store_pipe(sim_args, ip.num_sim_args - 4, child_read);
+	store_pipe(sim_args, ip.num_sim_args - 2, child_write);
+	
+	// Fork the process so the child can run the simulation
+	v << "  ";
+	v << term->blue << "Forking the process " << term->reset << ". . . ";
+	pid_t pid = fork();
+	if (pid == -1) {
+		term->failed_fork();
+		exit(EXIT_FORK_ERROR);
+	}
+	if (pid == 0) { // The child runs the simulation
+		v << "  ";
+		v << term->blue << "Checking that the simulation file exists and can be executed " << term->reset << ". . . ";
+		if (access(ip.sim_file, X_OK) == -1) {
+			term->failed_exec();
+			exit(EXIT_EXEC_ERROR);
+		}
+		term->done(v);
+		if (execv(ip.sim_file, sim_args) == -1) {
+			term->failed_exec();
+			exit(EXIT_EXEC_ERROR);
+		}
+	} else { // The parent pipes in the parameter set to run
+		v << term->blue << "Done: " << term->reset << "the child process's PID is " << pid << endl;
+		v << "  ";
+		v << term->blue << "Writing to the pipe " << term->reset << "(file descriptor " << parent_write << ") . . . ";
+		write_pipe(parent_write, parameters);
+		term->done(v);
+	}
+	
+	// Wait for the child to finish simulating
+	int status = 0;
+	waitpid(pid, &status, WUNTRACED);
+	if (WIFEXITED(status) == 0) {
+		term->failed_child();
+		exit(EXIT_CHILD_ERROR);
+	}
+	
+	
+	// Pipe in the simulation's score
+	//double max_score;
+	double score;
+	v << "  ";
+	v << term->blue << "Reading the pipe " << term->reset << "(file descriptor " << parent_read << ") . . . ";
+	read_pipe(parent_read, &score);
+	v << term->blue << "Done: " << term->reset << "(raw score " << score << " / " << 1 << ")" << endl;
+	double SRESscore;
+	
+	if (score >= 1){
+		SRESscore = 0;
+	}else {
+		SRESscore = 1 - score;
+	}
+    
+	//print_good_set(parameters, SRESscore);
+	// CLOSE PIPES
+	v << "  ";
+	v << term->blue << "Closing the the pipes " << term->reset << "(file descriptor " << parent_read << ", "<< parent_write << ", " << child_read << ", " << child_write<< ") . . . ";
+	if (close(parent_read) == -1) {
+		term->failed_pipe_read();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+	
+	if (close(parent_write) == -1) {
+		term->failed_pipe_write();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+
+	if (close(child_read) == -1) {
+		term->failed_pipe_write();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+	
+	if (close(child_write) == -1) {
+		term->failed_pipe_write();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+	term->done(v);
+	
+	// Free the simulation arguments
+	for (int i = 0; sim_args[i] != NULL; i++) {
+		mfree(sim_args[i]);
+	}
+	mfree(sim_args);
+	
+	
+	// libSRES requires scores from 0 to 1 with 0 being a perfect score so convert the simulation's score format into libSRES's
+	return SRESscore;
+}
+
+/*
+void print_good_set (double parameters[], double score) {
+    if (ip.print_good_sets && score <= ip.good_set_threshold) {
+        cout << term->blue << "  Found a good set " << term->reset << "(score " << score << ")" << endl;
+        ip.good_sets_stream << score << "," << parameters[0];
+        cout << score << "," << parameters[0];
+        for (int i = 1; i < DIM; i++) {
+            ip.good_sets_stream << "," << parameters[i];
+            cout << "," << parameters[i];
+        }
+        ip.good_sets_stream << endl;
+        cout << endl;
+    }
+}
+*/
+
+/* write_pipe writes the given parameter set to the given pipe
+	parameters:
+		fd: the file descriptor of the pipe to write to
+		parameters: the parameter set to pipe
+	returns: nothing
+	notes:
+	todo:
+*/
+void write_pipe (int fd, double parameters[]) {
+	write_pipe_int(fd, DIM); // Write the number of dimensions, i.e. parameters per set, being sent
+	write_pipe_int(fd, 1); // Write that one parameter set is being sent
+	if (write(fd, parameters, sizeof(double) * DIM) == -1) {
+		term->failed_pipe_write();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+	
+}
+
+/* write_pipe_int writes the given integer to the given pipe
+	parameters:
+		fd: the file descriptor of the pipe to write to
+		value: the integer to pipe
+	returns: nothing
+	notes:
+	todo:
+*/
+void write_pipe_int (int fd, int value) {
+	if (write(fd, &value, sizeof(int)) == -1) {
+		term->failed_pipe_write();
+		exit(EXIT_PIPE_WRITE_ERROR);
+	}
+}
+
+/* read_pipe reads the maximum score and the received score from the given pipe
+	parameters:
+		fd: the file descriptor of the pipe to write to
+		max_score: a pointer to store the maximum score the simulation could have received
+		score: a pointer to store the score the simulation actually received
+	returns: nothing
+	notes:
+	todo:
+*/
+void read_pipe (int fd, double* score) {
+	read_pipe_int(fd, score);
+}
+
+/* read_pipe_int writes an integer from the given pipe
+	parameters:
+		fd: the file descriptor of the pipe to write to
+		address: a pointer to store the received integer
+	returns: nothing
+	notes:
+	todo:
+*/
+void read_pipe_int (int fd, double* address) {
+	if (read(fd, address, sizeof(double)) == -1) {
+		term->failed_pipe_read();
+		exit(EXIT_PIPE_READ_ERROR);
+	}
+}
+
+/* close_if_open closes the given output file stream if it is open
+	parameters:
+		file: a pointer to the output file stream to close
+	returns: nothing
+	notes:
+	todo:
+		TODO: check if this function is still being used
+*/
+void close_if_open (ofstream& file) {
+	if (file.is_open()) {
+		file.close();
+	}
+}
+
---
+++ Downloads/circulating_clock_2016/bruteforce/source/main.cpp
@@ -0,0 +1,91 @@
+
+#include "main.hpp" // Function declarations
+
+#include "init.hpp"
+#include "macros.hpp"
+
+using namespace std;
+input_params ip;
+extern terminal* term; // Declared in init.cpp
+
+/* main is called when the program is run and performs all program functionality
+	parameters:
+		argc: the number of command-line arguments
+		argv: the array of command-line arguments
+	returns: 0 on success, a positive integer on failure
+	notes:
+		Main should only delegate functionality; let the functions it calls handle specific tasks. This keeps the function looking clean and helps maintain the program structure.
+	todo:
+*/
+int main (int argc, char** argv) {
+
+	// Initialize the program's terminal functionality and input parameters
+	init_terminal();
+	accept_input_params(argc, argv, ip);
+	init_verbosity(ip);
+	init_sim_args(ip);
+	
+	//Generate random parameter sets
+	double ** parameters;
+	generate_parameters(&parameters);
+	//Call to simulation
+	
+	// Free used memory, wrap up libSRES, etc.
+	#if defined(MEMTRACK)
+		print_heap_usage();
+	#endif
+	free_terminal();
+	reset_cout(ip);
+	return 0;
+}
+
+/* usage prints the usage information and, optionally, an error message and then exits
+	parameters:
+		message: an error message to print before the usage information (set message to NULL or "\0" to not print any error)
+	returns: nothing
+	notes:
+		This function exits after printing the usage information.
+		Note that accept_input_params in init.cpp handles actual command-line input and that this information should be updated according to that function.
+	todo:
+		TODO somehow free memory even with the abrupt exit
+*/
+void usage (const char* message) {
+	cout << endl;
+	bool error = message != NULL && message[0] != '\0';
+	if (error) {
+		cout << term->red << message << term->reset << endl << endl;
+	}
+	cout << "Usage: [-option [value]]. . . [--option [value]]. . ." << endl;
+	cout << "-f, --simulation         [filename]   : the relative filename of the simulation executable, default=../simulation/simulation" << endl;
+	cout << "-s, --seed               [int]        : the seed used in the evolutionary strategy (not simulations), min=1, default=time" << endl;
+	cout << "-a, --arguments          [N/A]        : every argument following this will be sent to the simulation" << endl;
+	cout << "-c, --no-color           [N/A]        : disable coloring the terminal output, default=unused" << endl;
+	cout << "-v, --verbose            [N/A]        : print detailed messages about the program state" << endl;
+	cout << "-q, --quiet              [N/A]        : hide the terminal output, default=unused" << endl;
+	cout << "-l, --licensing          [N/A]        : view licensing information (no simulations will be run)" << endl;
+	cout << "-h, --help               [N/A]        : view usage information (i.e. this)" << endl;
+	cout << endl << term->blue << "Example: ./sres-sampler " << term->reset << endl << endl;
+	if (error) {
+		exit(EXIT_INPUT_ERROR);
+	} else {
+		exit(EXIT_SUCCESS);
+	}
+}
+
+/* licensing prints the program's copyright and licensing information and then exits
+	parameters:
+	returns: nothing
+	notes:
+	todo:
+*/
+void licensing () {
+	cout << endl;
+	cout << "Stochastically ranked evolutionary strategy sampler for zebrafish segmentation" << endl;
+	cout << "Copyright (C) 2013 Ahmet Ay (aay@colgate.edu), Jack Holland (jholland@colgate.edu), Adriana Sperlea (asperlea@colgate.edu), Sebastian Sangervasi (ssangervasi@colgate.edu)" << endl;
+	cout << "This program comes with ABSOLUTELY NO WARRANTY" << endl;
+	cout << "This is free software, and you are welcome to redistribute it under certain conditions;" << endl;
+	cout << "You can use this code and modify it as you wish under the condition that you refer to the article: \"Short-lived Her proteins drive robust synchronized oscillations in the zebrafish segmentation clock\" (Development 2013 140:3244-3253; doi:10.1242/dev.093278)" << endl;
+	cout << endl;
+	exit(EXIT_SUCCESS);
+}
+
